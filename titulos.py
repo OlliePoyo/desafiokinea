73a50a7ccfa50282550a00a6eaf004bbc30ef3a3
Felipe Lucas <felipedelucas2000@gmail.com>
2022-06-16 12:59:04 -0300

    Moved titulos to dedicated file

---
+++ titulos.py
@@ -0,0 +1,92 @@
+from ambima_connect import AmbimaConnect
+import pandas as pd
+from dateutil.relativedelta import relativedelta
+from cal import AmbimaCalendar
+from datetime import date
+
+data_vna = AmbimaConnect('vna').content
+
+def get_dias_cupom(start_date:date, end_date:date):
+  """Retorna uma listagem de cupons entre duas datas, indo da data final até a inicial, de 6 em 6 meses"""
+  pgto_date_list = list()
+  pgto_date = end_date
+  # Gera da lista de datas a partir da data final, em incrementos de 6 meses
+  while pgto_date > start_date:
+    pgto_date_list.append(pgto_date)
+    pgto_date -= relativedelta(months=6)
+  # Inverte a ordem da lista para crescente
+  pgto_date_list=pgto_date_list[::-1]
+  return pgto_date_list
+
+def dias_uteis(start_date:date, end_date:date) -> int:
+  """O formato de data é date(AAAA, MM, DD), como date(2022, 2, 4) para 4 de fev de 2022"""
+  interval = AmbimaCalendar().get_working_days_delta(start_date, end_date)+1
+  return interval
+
+class LTN():
+  def __init__(self, data_venc, taxa, data_ref):
+    self.data_venc = data_venc
+    self.taxa = taxa
+    self.data_ref = data_ref
+
+    self.duration = dias_uteis(data_ref, data_venc)
+    self.pm = (data_venc - data_ref).days
+    self.pu = 1000/((1+taxa)**(self.duration/252))
+
+class NTNF():
+  def __init__(self, data_venc, taxa, data_ref):
+    self.data_venc = data_venc
+    self.taxa = taxa
+    self.data_ref = data_ref
+    
+    lista_datas = get_dias_cupom(data_ref, data_venc)
+    self.fluxo_pgto = pd.DataFrame(lista_datas, columns=['data_pgto'])
+    self.fluxo_pgto['cupom_semestral'] = [(1.1**(1/2)-1)*1000]*len(lista_datas)
+    self.fluxo_pgto['cupom_semestral'].iloc[-1] = self.fluxo_pgto['cupom_semestral'].iloc[-1] + 1000
+    self.fluxo_pgto['dias_uteis'] = self.fluxo_pgto['data_pgto'].apply(lambda x: dias_uteis(data_ref, x))
+    self.fluxo_pgto['dias_corridos'] = self.fluxo_pgto['data_pgto'].apply(lambda x: (x - data_ref).days)
+    self.fluxo_pgto['vp_pgto'] = self.fluxo_pgto.apply(lambda x: (x['cupom_semestral'])/((1+taxa)**(x['dias_uteis']/252)), axis=1)
+
+    self.pu = self.fluxo_pgto['vp_pgto'].sum()
+    self.duration = (self.fluxo_pgto['vp_pgto']*self.fluxo_pgto['dias_uteis']).sum()/self.pu
+    self.pm = (self.fluxo_pgto['cupom_semestral']*self.fluxo_pgto['dias_corridos']).sum()/self.fluxo_pgto['cupom_semestral'].sum()
+
+class NTNB():
+  def __init__(self, data_venc, taxa, data_ref):
+    self.data_venc = data_venc
+    self.taxa = taxa
+    self.data_ref = data_ref
+  # d15_base = data_liqui.replace(day=15)
+  # d15_proj = d15_base + relativedelta(months=1)
+  
+  # VNA deveria puxar dados de um banco, porém ainda não existe esse banco e o API tem só uma data em sandbox
+    self.vna = [x['vna'] for x in data_vna[0]['titulos'] if x['codigo_selic']=='760100'][0]
+
+    lista_datas = get_dias_cupom(data_ref, data_venc)
+    self.fluxo_pgto = pd.DataFrame(lista_datas, columns=['data_pgto'])
+    self.fluxo_pgto['fluxo'] = [(1.06**(1/2)-1)]*len(lista_datas)
+    self.fluxo_pgto['fluxo'].iloc[-1] = self.fluxo_pgto['fluxo'].iloc[-1] + 1
+    self.fluxo_pgto['dias_uteis'] = self.fluxo_pgto['data_pgto'].apply(lambda x: dias_uteis(data_ref, x))
+    self.fluxo_pgto['dias_corridos'] = self.fluxo_pgto['data_pgto'].apply(lambda x: (x - data_ref).days)
+    self.fluxo_pgto['vf_fluxo_taxa'] = self.fluxo_pgto.apply(lambda x: ((1+taxa)**(x['dias_uteis']/252)), axis=1)
+    self.fluxo_pgto['cotacao'] = self.fluxo_pgto.apply(lambda x: x['fluxo']/x['vf_fluxo_taxa'], axis=1)
+
+    self.fluxo_pgto['cupom_semestral'] = self.fluxo_pgto['fluxo']*self.vna
+    self.fluxo_pgto['vp_pgto'] = self.fluxo_pgto['cupom_semestral']/self.fluxo_pgto['vf_fluxo_taxa']
+
+    self.pu = self.fluxo_pgto['cotacao'].sum()*self.vna
+    self.duration = (self.fluxo_pgto['vp_pgto']*self.fluxo_pgto['dias_uteis']).sum()/self.fluxo_pgto['vp_pgto'].sum()
+    self.pm = (self.fluxo_pgto['cupom_semestral']*self.fluxo_pgto['dias_corridos']).sum()/self.fluxo_pgto['cupom_semestral'].sum()
+
+class LFT:
+  def __init__(self, data_venc, taxa, data_ref):
+    self.data_venc = data_venc
+    self.taxa = taxa
+    self.data_ref = data_ref
+    
+    self.duration = dias_uteis(data_ref, data_venc)
+    self.pm = (data_venc - data_ref).days
+
+    self.vna = [x['vna'] for x in data_vna[0]['titulos'] if x['codigo_selic']=='210100'][0]
+    self.cotacao = (1+taxa)**(-self.duration/252)
+    self.pu = self.vna*self.cotacao
--- utils.py
+++ utils.py
@@ -1,14 +1,6 @@
-from typing_extensions import NotRequired
-from cal import AmbimaCalendar
 import csv
-from datetime import date
 from typing import List, Dict, NoReturn
 
-
-def dias_uteis(start_date:date, end_date:date) -> int:
-  """O formato de data é date(AAAA, MM, DD), como date(2022, 2, 4) para 4 de fev de 2022"""
-  interval = AmbimaCalendar().get_working_days_delta(start_date, end_date)+1
-  return interval
 
 def check_error(acquired_value:float, expected_value:float) -> str:
   err = abs((acquired_value-expected_value)/expected_value)*100
