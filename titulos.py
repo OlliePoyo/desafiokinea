9076e146adae683f12151b9200a665fdba0f0bdd
Felipe Lucas <felipedelucas2000@gmail.com>
2022-06-17 03:42:57 -0300

    Changed date to string. Converted to date inside class

--- titulos.py
+++ titulos.py
@@ -2,7 +2,7 @@ from ambima_connect import AmbimaConnect
 import pandas as pd
 from dateutil.relativedelta import relativedelta
 from cal import AmbimaCalendar
-from datetime import date
+from datetime import date, datetime
 
 data_vna = AmbimaConnect('vna').content
 
@@ -23,14 +23,20 @@ def dias_uteis(start_date:date, end_date:date) -> int:
   interval = AmbimaCalendar().get_working_days_delta(start_date, end_date)+1
   return interval
 
+# class Titulo:
+  
+
 class LTN():
   def __init__(self, data_venc, taxa, data_ref):
     self.data_venc = data_venc
     self.taxa = taxa
     self.data_ref = data_ref
+    
+    d_data_ref=datetime.strptime(data_ref, "%Y-%m-%d").date()
+    d_data_venc=datetime.strptime(data_venc, "%Y-%m-%d").date()
 
-    self.duration = dias_uteis(data_ref, data_venc)
-    self.pm = (data_venc - data_ref).days
+    self.duration = dias_uteis(d_data_ref, d_data_venc)
+    self.pm = (d_data_venc - d_data_ref).days
     self.pu = 1000/((1+taxa)**(self.duration/252))
 
 class NTNF():
@@ -39,12 +45,15 @@ class NTNF():
     self.taxa = taxa
     self.data_ref = data_ref
     
-    lista_datas = get_dias_cupom(data_ref, data_venc)
+    d_data_ref=datetime.strptime(data_ref, "%Y-%m-%d").date()
+    d_data_venc=datetime.strptime(data_venc, "%Y-%m-%d").date()
+    
+    lista_datas = get_dias_cupom(d_data_ref, d_data_venc)
     self.fluxo_pgto = pd.DataFrame(lista_datas, columns=['data_pgto'])
     self.fluxo_pgto['cupom_semestral'] = [(1.1**(1/2)-1)*1000]*len(lista_datas)
     self.fluxo_pgto['cupom_semestral'].iloc[-1] = self.fluxo_pgto['cupom_semestral'].iloc[-1] + 1000
-    self.fluxo_pgto['dias_uteis'] = self.fluxo_pgto['data_pgto'].apply(lambda x: dias_uteis(data_ref, x))
-    self.fluxo_pgto['dias_corridos'] = self.fluxo_pgto['data_pgto'].apply(lambda x: (x - data_ref).days)
+    self.fluxo_pgto['dias_uteis'] = self.fluxo_pgto['data_pgto'].apply(lambda x: dias_uteis(d_data_ref, x))
+    self.fluxo_pgto['dias_corridos'] = self.fluxo_pgto['data_pgto'].apply(lambda x: (x - d_data_ref).days)
     self.fluxo_pgto['vp_pgto'] = self.fluxo_pgto.apply(lambda x: (x['cupom_semestral'])/((1+taxa)**(x['dias_uteis']/252)), axis=1)
 
     self.pu = self.fluxo_pgto['vp_pgto'].sum()
@@ -56,18 +65,22 @@ class NTNB():
     self.data_venc = data_venc
     self.taxa = taxa
     self.data_ref = data_ref
+    
+    d_data_ref=datetime.strptime(data_ref, "%Y-%m-%d").date()
+    d_data_venc=datetime.strptime(data_venc, "%Y-%m-%d").date()
   # d15_base = data_liqui.replace(day=15)
   # d15_proj = d15_base + relativedelta(months=1)
   
   # VNA deveria puxar dados de um banco, porém ainda não existe esse banco e o API tem só uma data em sandbox
-    self.vna = [x['vna'] for x in data_vna[0]['titulos'] if x['codigo_selic']=='760100'][0]
+  # Recomenda-se fazerteste de eficiencia. Talvez a chamada em api usando data como parametro seja preferível ao banco de dados
+    self.vna = [x['vna'] for x in [x['titulos'] for x in data_vna if x['data_referencia']==self.data_ref][0] if x['codigo_selic']=='760100'][0]
 
-    lista_datas = get_dias_cupom(data_ref, data_venc)
+    lista_datas = get_dias_cupom(d_data_ref, d_data_venc)
     self.fluxo_pgto = pd.DataFrame(lista_datas, columns=['data_pgto'])
     self.fluxo_pgto['fluxo'] = [(1.06**(1/2)-1)]*len(lista_datas)
     self.fluxo_pgto['fluxo'].iloc[-1] = self.fluxo_pgto['fluxo'].iloc[-1] + 1
-    self.fluxo_pgto['dias_uteis'] = self.fluxo_pgto['data_pgto'].apply(lambda x: dias_uteis(data_ref, x))
-    self.fluxo_pgto['dias_corridos'] = self.fluxo_pgto['data_pgto'].apply(lambda x: (x - data_ref).days)
+    self.fluxo_pgto['dias_uteis'] = self.fluxo_pgto['data_pgto'].apply(lambda x: dias_uteis(d_data_ref, x))
+    self.fluxo_pgto['dias_corridos'] = self.fluxo_pgto['data_pgto'].apply(lambda x: (x - d_data_ref).days)
     self.fluxo_pgto['vf_fluxo_taxa'] = self.fluxo_pgto.apply(lambda x: ((1+taxa)**(x['dias_uteis']/252)), axis=1)
     self.fluxo_pgto['cotacao'] = self.fluxo_pgto.apply(lambda x: x['fluxo']/x['vf_fluxo_taxa'], axis=1)
 
@@ -78,15 +91,45 @@ class NTNB():
     self.duration = (self.fluxo_pgto['vp_pgto']*self.fluxo_pgto['dias_uteis']).sum()/self.fluxo_pgto['vp_pgto'].sum()
     self.pm = (self.fluxo_pgto['cupom_semestral']*self.fluxo_pgto['dias_corridos']).sum()/self.fluxo_pgto['cupom_semestral'].sum()
 
-class LFT:
+class LFT():
   def __init__(self, data_venc, taxa, data_ref):
     self.data_venc = data_venc
     self.taxa = taxa
     self.data_ref = data_ref
     
-    self.duration = dias_uteis(data_ref, data_venc)
-    self.pm = (data_venc - data_ref).days
+    d_data_ref=datetime.strptime(data_ref, "%Y-%m-%d").date()
+    d_data_venc=datetime.strptime(data_venc, "%Y-%m-%d").date()
 
-    self.vna = [x['vna'] for x in data_vna[0]['titulos'] if x['codigo_selic']=='210100'][0]
+    self.duration = dias_uteis(d_data_ref, d_data_venc)
+    self.pm = (d_data_venc - d_data_ref).days
+
+    self.vna = [x['vna'] for x in [x['titulos'] for x in data_vna if x['data_referencia']==self.data_ref][0] if x['codigo_selic']=='210100'][0]
     self.cotacao = (1+taxa)**(-self.duration/252)
     self.pu = self.vna*self.cotacao
+
+class DIPerc():
+  def __init__(self, data_venc, taxa, data_ref):
+    self.data_venc = data_venc
+    self.taxa = taxa
+    self.data_ref = data_ref
+    
+    data_ref=datetime.strptime(data_ref, "%Y-%m-%d").date()
+    data_venc=datetime.strptime(data_venc, "%Y-%m-%d").date()
+
+class DISpread():
+  def __init__(self, data_venc, taxa, data_ref):
+    self.data_venc = data_venc
+    self.taxa = taxa
+    self.data_ref = data_ref
+    
+    data_ref=datetime.strptime(data_ref, "%Y-%m-%d").date()
+    data_venc=datetime.strptime(data_venc, "%Y-%m-%d").date()
+
+class IPCASpread():
+  def __init__(self, data_venc, taxa, data_ref):
+    self.data_venc = data_venc
+    self.taxa = taxa
+    self.data_ref = data_ref
+    
+    data_ref=datetime.strptime(data_ref, "%Y-%m-%d").date()
+    data_venc=datetime.strptime(data_venc, "%Y-%m-%d").date()
