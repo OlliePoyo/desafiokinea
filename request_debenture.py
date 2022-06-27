90765e1cb7bc7a6c1c582fa924254288329fcf4c
Felipe Lucas <felipedelucas2000@gmail.com>
2022-06-27 02:13:51 -0300

    Fixed missing quotation mark and added encoding

--- request_debenture.py
+++ request_debenture.py
@@ -5,7 +5,7 @@ import csv
 def save_to_csv(to_csv, filename='deb.csv'):
     keys = to_csv[0].keys()
 
-    with open(f'exports/agenda_debentures/{filename}', 'w', newline='') as output_file:
+    with open(f'exports/agenda_debentures/{filename}', 'w', newline='', encoding='utf-8') as output_file:
         dict_writer = csv.DictWriter(output_file, keys)
         dict_writer.writeheader()
         dict_writer.writerows(to_csv)
@@ -14,18 +14,18 @@ def save_to_csv(to_csv, filename='deb.csv'):
 def list_debentures():
     list_url = "https://data.anbima.com.br/debentures?page=1&size=999"
     with sync_playwright() as p: 
-        def handle_response(response): 
-        # the endpoint we are insterested in 
+        def handle_response(response):
+        # the endpoint we are insterested in
             if ("/web-bff/v1/debentures?" in response.url):
                 list_data = response.json()["content"]
-                save_to_csv(list_data, f'{list}.csv')
+                save_to_csv(list_data, 'list.csv')
 
         browser = p.chromium.launch()
         page = browser.new_page() 
 
-        page.on("response", handle_response) 
-        # really long timeout since it gets stuck sometimes 
-        page.goto(list_url, wait_until="networkidle") 
+        page.on("response", handle_response)
+
+        page.goto(list_url, wait_until="networkidle")
         page.wait_for_timeout(10000)
 
         page.context.close()
@@ -33,20 +33,20 @@ def list_debentures():
 
 
 def agenda_debenture(debenture:str):
-    agenda_url = f"https://data.anbima.com.br/debentures/{debenture}/agenda?page=1&size=999" 
+    agenda_url = f"https://data.anbima.com.br/debentures/{debenture}/agenda?page=1&size=999"
     with sync_playwright() as p: 
-        def handle_response(response): 
-        # the endpoint we are insterested in 
+        def handle_response(response):
+        # the endpoint we are insterested in
             if ("/web-bff/v1/debentures/" in response.url) and ("/agenda?" in response.url):
                 agenda_data = response.json()["content"]
                 save_to_csv(agenda_data, f'{debenture}.csv')
 
-        browser = p.chromium.launch() 
-        page = browser.new_page() 
+        browser = p.chromium.launch()
+        page = browser.new_page()
 
-        page.on("response", handle_response) 
-        # really long timeout since it gets stuck sometimes 
-        page.goto(agenda_url, wait_until="networkidle") 
+        page.on("response", handle_response)
+        
+        page.goto(agenda_url, wait_until="networkidle")
         page.wait_for_timeout(10000)
 
         page.context.close()
