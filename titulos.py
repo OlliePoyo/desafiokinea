d4634b24b2f1072dcfa58a33d4101a6de02db7fa
Felipe Lucas <felipedelucas2000@gmail.com>
2022-07-03 03:38:12 -0300

    Completed Priv (maybe only works w/ IPCA)

--- titulos.py
+++ titulos.py
@@ -1,8 +1,9 @@
-from ambima_connect import AmbimaConnect
 import pandas as pd
 from dateutil.relativedelta import relativedelta
 from cal import AmbimaCalendar
 from datetime import date, datetime
+from ambima_connect import AmbimaConnect
+from debenture import Debenture
 
 data_vna = AmbimaConnect('vna').content
 
@@ -22,9 +23,6 @@ def dias_uteis(start_date:date, end_date:date) -> int:
   """O formato de data é date(AAAA, MM, DD), como date(2022, 2, 4) para 4 de fev de 2022"""
   interval = AmbimaCalendar().get_working_days_delta(start_date, end_date)+1
   return interval
-
-# class Titulo:
-  
 
 class LTN():
   def __init__(self, data_venc, taxa, data_ref):
@@ -72,7 +70,7 @@ class NTNB():
   # d15_proj = d15_base + relativedelta(months=1)
   
   # VNA deveria puxar dados de um banco, porém ainda não existe esse banco e o API tem só uma data em sandbox
-  # Recomenda-se fazerteste de eficiencia. Talvez a chamada em api usando data como parametro seja preferível ao banco de dados
+  # Recomenda-se fazer teste de eficiencia. Talvez a chamada em api usando data como parametro seja preferível ao banco de dados
     self.vna = [x['vna'] for x in [x['titulos'] for x in data_vna if x['data_referencia']==self.data_ref][0] if x['codigo_selic']=='760100'][0]
 
     lista_datas = get_dias_cupom(d_data_ref, d_data_venc)
@@ -107,29 +105,40 @@ class LFT():
     self.cotacao = (1+taxa)**(-self.duration/252)
     self.pu = self.vna*self.cotacao
 
-class DIPerc():
-  def __init__(self, data_venc, taxa, data_ref):
-    self.data_venc = data_venc
-    self.taxa = taxa
-    self.data_ref = data_ref
-    
-    data_ref=datetime.strptime(data_ref, "%Y-%m-%d").date()
-    data_venc=datetime.strptime(data_venc, "%Y-%m-%d").date()
+class Priv():
+  def __init__(self, codigo_ativo):
+    self.codigo_ativo = codigo_ativo
 
-class DISpread():
-  def __init__(self, data_venc, taxa, data_ref):
-    self.data_venc = data_venc
-    self.taxa = taxa
-    self.data_ref = data_ref
-    
-    data_ref=datetime.strptime(data_ref, "%Y-%m-%d").date()
-    data_venc=datetime.strptime(data_venc, "%Y-%m-%d").date()
+    debenture = Debenture(codigo_ativo)
 
-class IPCASpread():
-  def __init__(self, data_venc, taxa, data_ref):
-    self.data_venc = data_venc
-    self.taxa = taxa
-    self.data_ref = data_ref
+    d_data_calc = date.today()
     
-    data_ref=datetime.strptime(data_ref, "%Y-%m-%d").date()
-    data_venc=datetime.strptime(data_venc, "%Y-%m-%d").date()
+    self.fluxo_pgto = debenture.agenda
+    self.fluxo_pgto['dias_uteis'] = self.fluxo_pgto['data_pgto'].apply(lambda x: dias_uteis(d_data_calc, datetime.strptime(x, "%d/%m/%Y").date()))
+    self.fluxo_pgto['juros_perc'] = self.fluxo_pgto.loc[0, 'taxa']*(-1)
+
+    self.fluxo_pgto.loc[0, 'remaining_perc'] = 1
+    self.fluxo_pgto.loc[0, 'amort'] = debenture.vne * self.fluxo_pgto.loc[0, 'taxa']
+    self.fluxo_pgto.loc[0, 'juros'] = debenture.vne * self.fluxo_pgto.loc[0, 'juros_perc']
+
+    for i in range(1, len(self.fluxo_pgto)):
+      self.fluxo_pgto.loc[i, 'remaining_perc'] = self.fluxo_pgto.loc[i-1, 'remaining_perc'] * (1-self.fluxo_pgto.loc[i, 'taxa'])
+      self.fluxo_pgto.loc[i, 'amort_perc'] = self.fluxo_pgto.loc[i-1, 'remaining_perc'] - self.fluxo_pgto.loc[i, 'remaining_perc']
+
+    self.fluxo_pgto.loc[1, 'amort'] = debenture.vne * self.fluxo_pgto.loc[1, 'amort_perc']
+    self.fluxo_pgto.loc[0, 'sld_dev'] = debenture.vne - self.fluxo_pgto.loc[0, 'amort']
+    self.fluxo_pgto.loc[1, 'sld_dev'] = self.fluxo_pgto.loc[0, 'sld_dev'] - self.fluxo_pgto.loc[1, 'amort']
+
+    for i in range(2, len(self.fluxo_pgto)):
+      self.fluxo_pgto.loc[i, 'amort'] = self.fluxo_pgto.loc[1, 'sld_dev'] * self.fluxo_pgto.loc[i, 'amort_perc']
+      self.fluxo_pgto.loc[i, 'sld_dev'] = self.fluxo_pgto.loc[i-1, 'sld_dev'] - self.fluxo_pgto.loc[i, 'amort']
+
+    for i in range(1, len(self.fluxo_pgto)):
+      self.fluxo_pgto.loc[i, 'juros'] = self.fluxo_pgto.loc[i, 'sld_dev'] * self.fluxo_pgto.loc[i, 'juros_perc']
+
+    self.fluxo_pgto['fv'] = self.fluxo_pgto['amort'] + self.fluxo_pgto['juros']
+    self.fluxo_pgto['fator_desc'] = (debenture.juros_taxa+1)**(self.fluxo_pgto['dias_uteis']/252)
+    self.fluxo_pgto['pv'] = self.fluxo_pgto['fv']/self.fluxo_pgto['fator_desc']
+
+    self.pm = (self.fluxo_pgto['dias_uteis']*self.fluxo_pgto['fv']).sum()/self.fluxo_pgto['fv'].sum()
+    self.duration = (self.fluxo_pgto['dias_uteis']*self.fluxo_pgto['pv']).sum()/self.fluxo_pgto['pv'].sum()
