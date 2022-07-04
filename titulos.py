68fd004aa47a35cb83f44297bdfe079e699474ca
Felipe Lucas <felipedelucas2000@gmail.com>
2022-07-04 13:28:34 -0300

    Added caracteristicas to self in Priv

--- titulos.py
+++ titulos.py
@@ -115,7 +115,9 @@ class Priv():
 
     debenture = Debenture(codigo_ativo)
 
-    self.data_ref = datetime.strptim(debenture.inicio_rent, "$d/%m/%Y").date()
+    self.caracteristicas = debenture.caracteristicas
+
+    self.data_ref = datetime.strptime(debenture.inicio_rent, "%d/%m/%Y").date()
     
     self.fluxo_pgto = debenture.agenda
     self.fluxo_pgto['dias_uteis'] = self.fluxo_pgto['data_pgto'].apply(lambda x: dias_uteis(self.data_ref, datetime.strptime(x, "%d/%m/%Y").date()))
