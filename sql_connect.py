549492dbcd4d1508881ce518eae60c7dad4dd832
Felipe Lucas <felipedelucas2000@gmail.com>
2022-07-05 15:18:55 -0300

    Added select_sql

--- sql_connect.py
+++ sql_connect.py
@@ -16,3 +16,18 @@ def send_to_sql(sql_query:str):
               connection.commit()
   except Error as e:
       print(e)
+
+@retry(delay=1, tries=4, jitter=2)
+def select_sql(sql_query:str):
+  try:
+      with connect(
+          host=HOST,
+          user=USER,
+          password=PASSWORD,
+          database=DATABASE,
+      ) as connection:
+          with connection.cursor() as cursor:
+              cursor.execute(sql_query)
+              return cursor.fetchall()
+  except Error as e:
+      print(e)
